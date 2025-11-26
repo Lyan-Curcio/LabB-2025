83bd80ccd8bc0914466e13653675c7eaa6d9ef9e
Ergioz <monachino.lorenzo.italy@gmail.com>
2025-11-26 17:25:52 +0100

    Ripristino file sorgenti e attivazione gitignore

---
+++ src/main/java/com/lab_b/Logincon.java
@@ -0,0 +1,43 @@
+package com.lab_b;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import java.io.IOException;
+
+public class Logincon {
+
+    @FXML private TextField UserID;
+    @FXML private PasswordField Password;
+    @FXML private Button BLogin;
+    @FXML private Button ButtonReturn;
+
+    @FXML
+    void BtnclickLog(ActionEvent event) throws IOException {
+        CheckLogin();   
+    }
+
+    @FXML
+    void BtnReturn(ActionEvent event) throws IOException {
+        App m = new App();
+        m.changeScene("Benvenuto.fxml");
+    }
+    
+    private void CheckLogin() throws IOException {
+        String uid = UserID.getText();
+        String pwd = Password.getText();
+        App m = new App();
+
+        // Simulazione login (da sostituire con DB in futuro)
+        if("Sergio".equals(uid) && "123".equals(pwd)) {
+            System.out.println("Successo");
+            // m.changeScene("Libri.fxml"); // Decommentare quando esiste Libri.fxml
+        } else if (uid.isEmpty() || pwd.isEmpty()) {
+            System.out.println("Per favore inserire i dati");
+        } else {
+            System.out.println("Nome utente o password errati");
+        }
+    }
+}
---
+++ src/main/java/com/lab_b/Regcontrols.java
@@ -0,0 +1,38 @@
+package com.lab_b;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Regcontrols {
+
+    @FXML private TextField Tfnome;
+    @FXML private TextField Tfcognome;
+    @FXML private TextField TfUserid;
+    @FXML private TextField TfCodiceFiscale;
+    @FXML private TextField TfEmail;
+    @FXML private TextField TfCreaPassword;
+    @FXML private TextField TfConfermaPassword;
+
+    @FXML
+    void BtnClick(ActionEvent event) throws Exception {
+        String password = TfCreaPassword.getText();
+        String cPassword = TfConfermaPassword.getText();
+
+        if(!password.equals(cPassword)) {
+            System.out.println("Le password non coincidono");
+            return;
+        }
+        
+        // Qui andrà il codice per salvare nel DB PostgreSQL
+        // Per ora stampiamo solo a video
+        System.out.println("Tentativo registrazione: " + Tfnome.getText() + " " + Tfcognome.getText());
+    }
+
+    @FXML
+    void BtnReturn(ActionEvent event) throws IOException {
+        App m = new App();
+        m.changeScene("Benvenuto.fxml");
+    }
+}
