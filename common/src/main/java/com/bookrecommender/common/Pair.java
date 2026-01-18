7244434210ca1c8c8c48d069439159ef728fc005
LyroyTheToad <lyroy.thetoad@gmail.com>
2026-01-18 11:57:56 +0100

    Fixato Pair non serializzabile

--- common/src/main/java/com/bookrecommender/common/Pair.java
+++ common/src/main/java/com/bookrecommender/common/Pair.java
@@ -1,3 +1,7 @@
 package com.bookrecommender.common;
 
-public record Pair<A, B>(A first, B second) {}
+import java.io.Serializable;
+import java.rmi.Remote;
+
+// I campi del pair sono fissati a 'Serializable' e 'Remote' perché viene usato solo in questo modo
+public record Pair<A extends Serializable, B extends Remote>(A first, B second) implements Serializable {}
