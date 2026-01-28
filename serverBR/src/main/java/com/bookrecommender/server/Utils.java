7e13462b5f63c6053ebba6df99cce99890068285
Lyan-Curcio <lyan.curcio@gmail.com>
2026-01-28 18:29:31 +0100

    Aggiunto javadoc

--- serverBR/src/main/java/com/bookrecommender/server/Utils.java
+++ serverBR/src/main/java/com/bookrecommender/server/Utils.java
@@ -7,7 +7,23 @@ import com.bookrecommender.server.queries.BookQueries;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
 
+
+/**
+ * Classe di utility generale.
+ *
+ * @author Lorenzo Monachino 757393 VA
+ * @author Lyan Curcio 757579 VA
+ * @author Sergio Saldarriaga 757394 VA
+ * @author Nash Guizzardi 756941 VA
+ */
 public class Utils {
+
+    /**
+     * Costruisce una lista di {@link SuggestionWithBooks} da una lista di {@link Suggestion}
+     *
+     * @param suggestions la lista di {@link Suggestion} da cui costruire la lista di {@link SuggestionWithBooks}
+     * @return la lista di {@link SuggestionWithBooks}
+     */
     static public LinkedList<SuggestionWithBooks> suggWithBooksFromSugg(LinkedList<Suggestion> suggestions) {
         return suggestions.stream().map(s -> new SuggestionWithBooks(
             s.userId,
